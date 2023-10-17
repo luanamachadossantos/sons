e1e558eb5afe3a3a0b40ec7f1db2dc3615421db1
luanamachadossantos <111470554+luanamachadossantos@users.noreply.github.com>
2023-10-17 11:05:53 -0300

    comentarios

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
+//criando/declarando a função TocaSomPom
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
+//chamando/invocando a função TocaSomPom pelo clique do botão pom 
  document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-}
- document.querySelector('.tecla_clap').onclick = tocaSomClap;
+//criando referencia constante listaDeTeclas buscando todos os itens pela clase .tecla
+const listaDeTeclas = document.querySelectorAll('.tecla');
+//invocando a função TocaSomPom a partir do intem "0", 1 botão
+listaDeTeclas [0].onclick = tocaSomPom;
