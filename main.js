21ac238e9110856a6f07c457e9de978b3e3c23b2
luanamachadossantos <111470554+luanamachadossantos@users.noreply.github.com>
2023-10-24 11:28:32 -0300

    )oik

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
-//criando/declarando a função TocaSomPom
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+//criando/declarando a função TocaSom com pârametro iDElementoAudio 
+function tocaSom(iDElementoAudio){
+    document.querySelector(iDElementoAudio).play();
 }
-//chamando/invocando a função TocaSomPom pelo clique do botão pom 
- document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
 //criando referencia constante listaDeTeclas buscando todos os itens pela clase .tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
-//invocando a função TocaSomPom a partir do intem "0", 1 botão
-listaDeTeclas [0].onclick = tocaSomPom;
+
+
 
 //criando referência variável "contador" iniciando = 0 
 let contador = 0;
 
 // estrutura de repetição "while" significa "enquanto algo acontecer"
-while(contador < 9 ){
-    listaDeTeclas[contador].onclick = tocaSomPom;
+while(contador < listaDeTeclas .length){
+    const efeito = listaDeTeclas [contador]. classList [1];
+    const idAudio = "#som_"+ efeito; 
+    listaDeTeclas[contador].onclick = (function) (){
+        tocaSom (idAudio)
+    }
     contador = contador + 1; 
     console.log(contador);
 
